154807659dd536ab371024783ec07ee96884a4f5
Reload0213 <dldudwn7227@gmail.com>
2025-01-08 19:07:56 +0900

    git init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+// import UI from './ui';
+
+export const metadata = {
+    title: 'TMDBFLIX',
+    description: 'Netflix clone using TMDBFLIX API',
+};
+
+export default function Home() {
+    return <main>Netflix Clone</main>;
+}
